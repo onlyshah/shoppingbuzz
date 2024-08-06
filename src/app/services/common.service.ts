1d8b99b486798bf10149e879ec9c3c2d113613d4
Nikunj0014 <119851905+Nikunj0014@users.noreply.github.com>
2024-08-07 00:24:59 +0530

    header

--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, Subject, map, tap } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -19,123 +19,132 @@ export class CommonService implements OnInit {
 
   private orderStatussubject = new BehaviorSubject<any[]>([]);
   public orderStatussubjec$: Observable<any[]> = this.orderStatussubject.asObservable();
- 
+
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Accept': '*/*'
+  });
+
   constructor(private http: HttpClient) {}
-  
 
- 
   ngOnInit(): void {}
-  getCatgeory() :Observable<any>{
-    return this.http.get(environment.baseUrl + 'getcategory');
-  }
-  getsubCatgeory():Observable<any> {
-    return this.http.get(environment.baseUrl + 'viewSubcategory');
-  }
-  getall():Observable<any> {
-    return this.http.get(environment.baseUrl + 'getall');
-  }
-  getcarousel():Observable<any> {
-    return this.http.get(environment.baseUrl + 'getcarousle');
-  }
-  getCardcarousel():Observable<any> {
-    return this.http.get(environment.baseUrl + 'getcardcarousel');
-  }
-  addtocart(data: any):Observable<any> {
-    return this.http.post(environment.baseUrl + 'cart/addtocart', data);
-  }
- 
-  onUpdatecart(data: any):Observable<any> {
-    return this.http.put(environment.baseUrl + 'cart/updatecart', data);
-  }
-  getproduct():Observable<any> {
-    return this.http.get(environment.baseUrl + 'viewProducts');
-  }
-  addtowishlist(data: any):Observable<any> {
-    return this.http.post(
-      environment.baseUrl + 'wishlist/createwishlist',
-      data
-    );
-  }
-  getproducttocart(userId: any):Observable<any> {
-    return this.http.get(environment.baseUrl + 'cart/getcart/' + userId)
-    .pipe(map((response: any) => {
-      const productCount = response.productCount; // Assuming response contains productCount
-      this.cardCountSubject.next(productCount);
-      console.log('Updated cartState', this.cardCountSubject.value);
-      return response;
-    }));
+
+  private getOptions(): { headers: HttpHeaders } {
+    return { headers: this.headers };
   }
 
-  getwishlist(userId: any):Observable<any> {
-    return this.http.get(
-      environment.baseUrl + 'wishlist/getwishlist/' + userId)
+  getCategory(): Observable<any> {
+    return this.http.get(environment.baseUrl + 'getcategory', this.getOptions());
+  }
+
+  getSubCategory(): Observable<any> {
+    return this.http.get(environment.baseUrl + 'viewSubcategory', this.getOptions());
+  }
+
+  getAll(): Observable<any> {
+    return this.http.get(environment.baseUrl + 'getall', this.getOptions());
+  }
+
+  getCarousel(): Observable<any> {
+    return this.http.get(environment.baseUrl + 'getcarousle', this.getOptions());
+  }
+
+  getCardCarousel(): Observable<any> {
+    return this.http.get(environment.baseUrl + 'getcardcarousel', this.getOptions());
+  }
+
+  addToCart(data: any): Observable<any> {
+    return this.http.post(environment.baseUrl + 'cart/addtocart', data, this.getOptions());
+  }
+
+  onUpdateCart(data: any): Observable<any> {
+    return this.http.put(environment.baseUrl + 'cart/updatecart', data, this.getOptions());
+  }
+
+  getProduct(): Observable<any> {
+    return this.http.get(environment.baseUrl + 'viewProducts', this.getOptions());
+  }
+
+  addToWishlist(data: any): Observable<any> {
+    return this.http.post(environment.baseUrl + 'wishlist/createwishlist', data, this.getOptions());
+  }
+
+  getProductToCart(userId: any): Observable<any> {
+    return this.http.get(environment.baseUrl + 'cart/getcart/' + userId, this.getOptions())
       .pipe(map((response: any) => {
-        const ListCount = response.ListCount; // Assuming response contains productCount
-        this.wishlistCountSubject.next(ListCount);
-        console.log('Updated cartState', this.wishlistCountSubject.value);
+        const productCount = response.productCount;
+        this.cardCountSubject.next(productCount);
+        console.log('Updated cartState', this.cardCountSubject.value);
         return response;
       }));
-    }
-  
-  orderCreate(order:any):Observable<any>{
-    return this.http.post(environment.baseUrl+'order/createorder',order)
   }
-  
-  getOrder(userId:any):Observable<any>{
-    return this.http.get(environment.baseUrl+'order/getorderbyuserId/'+userId)
+
+  getWishlist(userId: any): Observable<any> {
+    return this.http.get(environment.baseUrl + 'wishlist/getwishlist/' + userId, this.getOptions())
+      .pipe(map((response: any) => {
+        const listCount = response.ListCount;
+        this.wishlistCountSubject.next(listCount);
+        console.log('Updated wishlistState', this.wishlistCountSubject.value);
+        return response;
+      }));
   }
-  deleteWishlistprod(userId:any ,productId:any) :Observable<any>{
-    console.log(productId)
-    return this.http.delete(environment.baseUrl+'wishlist/deletewishlist/'+userId+ '/' + productId)
-    .pipe(map((response:any) => {
-      // store user details and jwt token in local storage to keep user logged in between page refreshes
-      this.subjectOBJ.asObservable();
-      this.subjectOBJ.next(response);
-      return response;
-  }));
+
+  orderCreate(order: any): Observable<any> {
+    return this.http.post(environment.baseUrl + 'order/createorder', order, this.getOptions());
   }
-  deletecartItem(userId:any ,productId:any):  Observable<any>{
-    return this.http.delete(environment.baseUrl+'cart/deletecartitem/'+ userId + '/' + productId).pipe(map((response:any) => {
-      // store user details and jwt token in local storage to keep user logged in between page refreshes
-      this.subjectOBJ.asObservable();
-      this.subjectOBJ.next(response);
-      return response;
-  }));
+
+  getOrder(userId: any): Observable<any> {
+    return this.http.get(environment.baseUrl + 'order/getorderbyuserId/' + userId, this.getOptions());
   }
-  deletecartItemByuserId(userId:any):  Observable<any>{
-    return this.http.delete(environment.baseUrl+'cart/deleteByuId/'+ userId).pipe(map((response:any) => {
-      // store user details and jwt token in local storage to keep user logged in between page refreshes
-      this.subjectOBJ.asObservable();
-      this.subjectOBJ.next(response);
-      return response;
-  }));
+
+  deleteWishlistProd(userId: any, productId: any): Observable<any> {
+    console.log(productId);
+    return this.http.delete(environment.baseUrl + 'wishlist/deletewishlist/' + userId + '/' + productId, this.getOptions())
+      .pipe(map((response: any) => {
+        this.subjectOBJ.next(response);
+        return response;
+      }));
   }
-  SearchData(data:any): Observable<any> {
-    return this.http.get(`${environment.baseUrl}search?searchValue=${data}`)
-    .pipe(map((response:any) => {
-      // store user details and jwt token in local storage to keep user logged in between page refreshes
-      // this.searchSubject.asObservable();
-      // this.searchSubject.next(response);
-      // return response;
-      this.searchSubject.next(response);
-      return response;
-  }));
+
+  deleteCartItem(userId: any, productId: any): Observable<any> {
+    return this.http.delete(environment.baseUrl + 'cart/deletecartitem/' + userId + '/' + productId, this.getOptions())
+      .pipe(map((response: any) => {
+        this.subjectOBJ.next(response);
+        return response;
+      }));
   }
+
+  deleteCartItemByUserId(userId: any): Observable<any> {
+    return this.http.delete(environment.baseUrl + 'cart/deleteByuId/' + userId, this.getOptions())
+      .pipe(map((response: any) => {
+        this.subjectOBJ.next(response);
+        return response;
+      }));
+  }
+
+  searchData(data: any): Observable<any> {
+    return this.http.get(`${environment.baseUrl}search?searchValue=${data}`, this.getOptions())
+      .pipe(map((response: any) => {
+        this.searchSubject.next(response);
+        return response;
+      }));
+  }
+
   getSearchObservable(): Observable<any> {
     return this.searchSubject.asObservable();
   }
+
   updateSearchResults(results: any[]): void {
-    console.log("*****",results)
+    console.log("*****", results);
     this.searchSubject.next(results);
-    console.log(results)
+    console.log(results);
   }
-  updateOrderStatus(orderId:any,data:any){
-    return this.http.put(environment.baseUrl+'order/update-status/'+ orderId ,data).pipe(map((response:any) => {
-      // store user details and jwt token in local storage to keep user logged in between page refreshes
-      this.orderStatussubject.asObservable();
-      this.orderStatussubject.next(response);
-      return response;
-  }));
+
+  updateOrderStatus(orderId: any, data: any): Observable<any> {
+    return this.http.put(environment.baseUrl + 'order/update-status/' + orderId, data, this.getOptions())
+      .pipe(map((response: any) => {
+        this.orderStatussubject.next(response);
+        return response;
+      }));
   }
- 
 }
