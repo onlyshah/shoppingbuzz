90571b9276001d5055cc45b5f089a54f5adbb8d1
Nikunj0014 <119851905+Nikunj0014@users.noreply.github.com>
2024-08-21 00:39:47 +0530

    allok

--- src/app/pages/page-not-found-component/page-not-found.component.ts
+++ src/app/pages/page-not-found-component/page-not-found.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-page-not-found.component',
+  selector: 'app-page-not-found',
   standalone: true,
   imports: [],
   templateUrl: './page-not-found.component.html',
