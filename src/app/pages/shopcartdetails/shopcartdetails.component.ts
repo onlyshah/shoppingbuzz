6919d57ed7ff4521cf20ac1e86b5da385fb8b6ed
Nikunj0014 <119851905+Nikunj0014@users.noreply.github.com>
2024-08-20 21:05:08 +0530

    okall

--- src/app/pages/shopcartdetails/shopcartdetails.component.ts
+++ src/app/pages/shopcartdetails/shopcartdetails.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 import { first ,Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { CommonService } from 'src/app/services/common.service';
@@ -24,8 +26,11 @@ export class ShopcartdetailsComponent  implements OnInit{
   quantityplus:any;
   quantityminus:any;
   mySubscription:Subscription
+  status:any
  constructor(private comApi:CommonService ,private auth:AuthService,
   private route: Router , private router: ActivatedRoute ,
+  private spinner: NgxSpinnerService, // Inject the spinner service
+  private toster : ToastrService
 ) {
   this.route.routeReuseStrategy.shouldReuseRoute = function () {
     return false;
@@ -41,58 +46,68 @@ export class ShopcartdetailsComponent  implements OnInit{
   }
 
   ngOnInit(): void {
-   this.comApi.getproducttocart(this.userId).subscribe((response:any)=>{
-    this.data = response.products    ;
-    console.log('data',this.data)
-    this.updateprices =0;
-    this.data.forEach((el:any) => {
-      this.productquantity = el.quantity
-      this.updateprices +=el.quantity*el.productId.price
-      //console.log(this.updateprices)
+    this.spinner.show(); // Show spinner when data fetching starts
+    this.comApi.getproducttocart(this.userId).subscribe((response: any) => {
+      this.data = response.products;
+      console.log('data', this.data);
+      this.updateprices = 0;
+      this.data.forEach((el: any) => {
+        this.productquantity = el.quantity;
+        this.updateprices += el.quantity * el.productId.price;
+        // console.log(this.updateprices)
+      });
+      this.spinner.hide(); // Hide spinner when data fetching completes
+    }, (error) => {
+      this.status = error.status
+      if(error.status === 404){
+       this.toster.error(error.error.message)
+      }
+     // console.error(error.status);
+      this.spinner.hide(); // Hide spinner if there's an error
     });
-   
-   
-
-    
-   })
-  
   }
  
-  quantity(value:any ,index:any ,productId:any ,proid:any){
-    if(value == 'max'){
-    this.data[index].quantity =this.data[index].quantity+1
-    this.quantityplus = this.data[index].quantity
-    console.log('id',proid ,this.userId , this.data[index].quantity)
-    let data = {
-        "userId": this.userId,
-        "products": {
-        "productId": proid,
-        "quantity": this.quantityplus
-    }
-    
-    }
-    this.comApi.onUpdatecart(data).subscribe((res:any)=>{
-      let data = res
-      console.log('cart update',data)
-    })
-    }
-    else if(value == 'min'){
-      this.data[index].quantity =this.data[index].quantity-1
-      this.quantityminus = this.data[index].quantity  
+  quantity(value: any, index: any, productId: any, proid: any) {
+    this.spinner.show(); // Show spinner when updating cart
+    if (value === 'max') {
+      this.data[index].quantity += 1;
+      this.quantityplus = this.data[index].quantity;
+      console.log('id', proid, this.userId, this.data[index].quantity);
       let data = {
         "userId": this.userId,
         "products": {
-        "productId": proid,
-        "quantity": this.quantityminus
+          "productId": proid,
+          "quantity": this.quantityplus
+        }
+      };
+      this.comApi.onUpdatecart(data).subscribe((res: any) => {
+        let data = res;
+        console.log('cart update', data);
+        this.spinner.hide(); // Hide spinner after update
+      }, (error) => {
+        console.error('Error updating cart:', error);
+        this.spinner.hide(); // Hide spinner if there's an error
+      });
+    } else if (value === 'min') {
+      this.data[index].quantity -= 1;
+      this.quantityminus = this.data[index].quantity;
+      let data = {
+        "userId": this.userId,
+        "products": {
+          "productId": proid,
+          "quantity": this.quantityminus
+        }
+      };
+      this.comApi.onUpdatecart(data).subscribe((res: any) => {
+        let data = res;
+        console.log('cart update', data);
+        this.spinner.hide(); // Hide spinner after update
+      }, (error) => {
+        console.error('Error updating cart:', error);
+        this.spinner.hide(); // Hide spinner if there's an error
+      });
     }
-    
-    }
-    this.comApi.onUpdatecart(data).subscribe((res:any)=>{
-      let data = res
-      console.log('cart update',data)
-    })
-    }
-    this.doTotalPrice(index ,value)
+    this.doTotalPrice(index, value);
   }
   doTotalPrice(index:any ,value:any) {
     if(value == 'max'){
@@ -111,32 +126,43 @@ export class ShopcartdetailsComponent  implements OnInit{
   onUpdadte(value:any){}
 
   // from  cart to wishlist
-  movetoWishList(productId:any){
-    var value= {
-      "userId":this.userId,
-      "List":{
-        "productId":productId,
-        
-        
+   
+  movetoWishList(productId: any) {
+    this.spinner.show(); // Show spinner when moving to wishlist
+    var value = {
+      "userId": this.userId,
+      "List": {
+        "productId": productId,
       }
-    }
-    // console.log(value)
-    this.comApi.addtowishlist(value).subscribe((res:any)=>{
+    };
+    this.comApi.addtowishlist(value).subscribe((res: any) => {
       let wishlist = res;
-      console.log('wishlist',wishlist)
-    })
-    this.deletecartItem(productId)
-
-  }
-  deletecartItem(productId:any){
-    console.log('delete',productId ,this.userId) 
-    this.comApi.deletecartItem(this.userId,productId).pipe(first())
-    .subscribe({
-      next: (res: any) => {
-         console.log('deleted',res)
-      },
+      console.log('wishlist', wishlist);
+      this.spinner.hide(); // Hide spinner after adding to wishlist
+    },(error) => {
+        this.status = error.status
+        if(error.status === 404){
+         this.toster.error(error.error.message)
+        }
+      this.spinner.hide(); // Hide spinner if there's an error
     });
-
+    this.deletecartItem(productId);
+  }
+  
+  deletecartItem(productId: any) {
+    console.log('delete', productId, this.userId);
+    this.spinner.show(); // Show spinner when deleting cart item
+    this.comApi.deletecartItem(this.userId, productId).pipe(first())
+      .subscribe({
+        next: (res: any) => {
+          console.log('deleted', res);
+          this.spinner.hide(); // Hide spinner after deletion
+        },
+        error: (error) => {
+          console.error('Error deleting cart item:', error);
+          this.spinner.hide(); // Hide spinner if there's an error
+        }
+      });
   }
    
   
