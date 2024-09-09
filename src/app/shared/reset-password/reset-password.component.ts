0aa42ed5163458b8133d5e4aca50ff5e1bb156b0
unknown <shahnikunjsbit@gmail.com>
2024-09-10 00:52:48 +0530

    okall...

--- src/app/shared/reset-password/reset-password.component.ts
+++ src/app/shared/reset-password/reset-password.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
 import { CommonService } from 'src/app/services/common.service';
-import { Buffer } from 'buffer';
+
 
 @Component({
   selector: 'app-reset-password',
@@ -20,7 +21,8 @@ export class ResetPasswordComponent implements OnInit {
     private comApi: CommonService,
     private auth: AuthService,
     private fb: FormBuilder,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private toster : ToastrService
   ) {
     // Initialize the form with only the password field
     this.resetPasswordForm = this.fb.group({
@@ -29,12 +31,23 @@ export class ResetPasswordComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      const resetLink:any = params.get('/reset-password/');
-      const parts = resetLink.split('/reset-password/')[1];
-      console.log('parts',parts)
+      this.route.params.subscribe((response: any) => {
+        const resetLink = '' + response.token; // This should be the full URL with token and email part
+        const [token, emailHex] = resetLink.split('+'); // Assuming '+' is the delimiter
+        try {
+            const decodedEmail = atob(emailHex); // Decode email part if it was base64 encoded
+            console.log('Decoded email:', decodedEmail);
+            console.log('Reset token:', token);
+            this.token = token
+            this.email = decodedEmail
+        } catch (e) {
+            console.error('Error decoding Base64 string:', e);
+        }
+       
+      });
+    
      
-    });
+
     
   }
 
@@ -47,13 +60,15 @@ export class ResetPasswordComponent implements OnInit {
       const resetData = {
         token: this.token,
         password: this.resetPasswordForm.value.password,
+        email:this.email
       };
 
       // Call the backend API with the token and new password
       this.auth.resetuserPassword(resetData).subscribe(
         (res: any) => {
-          console.log(res);
+          console.log(res);   
           this.router.navigateByUrl('/login');
+          this.toster.error(res.message)
         },
         (error) => {
           console.error('Error resetting password:', error);
